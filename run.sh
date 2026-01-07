c87f73bc6d1dbf60e16804e22228bb9f9fe56aee
MaloOllivier <malo.ollivier@etudiant.uni-rennes.fr>
2026-01-07 21:36:57 +0000

    ajout des dossiers fichiers et resultat, run.sh amélliorer

--- run.sh
+++ run.sh
@@ -13,9 +13,22 @@ docker run -dit --name $TRANSFERT -v $VOLUME:/data $IMAGE >> $LOGS
 docker cp "DEPTS" $TRANSFERT:/data/"DEPTS" >> $LOGS
 docker cp "REGIONS" $TRANSFERT:/data/"REGIONS" >> $LOGS
 docker cp "nettoyage.php" $TRANSFERT:/data/nettoyage.php >> $LOGS
-./Convertisseur_CSV.sh
+cd fichiers/
+for FICH in *.xlsx; do
+  NOMFICH=$(basename "$FICH" .xlsx)
+  echo --------------
+  echo "Conversion de '$FICH' --> '$NOMFICH.csv'"
+
+  # Copie du fichier dans le container TRANSFERT
+  docker cp "$FICH" $TRANSFERT:/data/"$FICH" >> $LOGS
+
+  # Conversion des xlsx en csv
+  docker run --rm -v $VOLUME:/data $IMAGE sh -c "ssconvert \"/data/$FICH\" \"/data/$NOMFICH.csv\""
+
+  echo "✓ \"$NOMFICH\" converti en csv"
+done
 # faut recup le nom du fichier
-docker run --rm -v $VOLUME:/data $IMAGE php /data/nettoyage.php /data/test.csv
-docker cp $TRANSFERT:/data/"test.csv" "$CHEMIN" >> $LOGS
+docker run --rm -v $VOLUME:/data $IMAGE php /data/nettoyage.php /data/$NOMFICH.csv
+docker cp $TRANSFERT:/data/"$NOMFICH.csv" "$CHEMIN/resultat/" >> $LOGS
 # Suppression du docker de TRANSFERT
 docker rm -f $TRANSFERT >> $LOGS
