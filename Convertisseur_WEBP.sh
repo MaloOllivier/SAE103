54e70106e7848b2a47b614565a474448a390202e
Gleleu <gaetanleleu29@gmail.com>
2026-01-08 14:53:59 +0100

    mise a jour du convertisseur webp et ajour de nettoyeur pour les textes (vides)

--- Convertisseur_WEBP.sh
+++ Convertisseur_WEBP.sh
@@ -3,7 +3,7 @@
 # Nom du volume
 VOLUME="SAE103_MALOOLLIVIER"
 # Image
-IMAGE="sae103-png2webp"
+IMAGE="sae103-jpg2webp"
 # Chemain du dossier
 CHEMIN=$(pwd)
 # Logs
@@ -17,15 +17,15 @@ TRANSFERT="temporaire_$(date +%s%N)"
 docker run -dit --name $TRANSFERT -v $VOLUME:/data $IMAGE >> $LOGS
 
 # Boucle pour tous les fichiers en .webp présent dans le dossier
-for FICH in *.png; do
-  NOMFICH=$(basename "$FICH" .png)
+for FICH in *.jpg; do
+  NOMFICH=$(basename "$FICH" .jpg)
   echo --------------
   echo "Conversion de '$FICH' --> '$NOMFICH.webp'"
 
   # Copie du fichier dans le volume par l'intermidère du conteneur de transfert
   docker cp "$FICH" $TRANSFERT:/data/"$FICH" >> $LOGS
 
-  # Conversion des png en webp
+  # Conversion des jpg en webp
   docker run --rm -v $VOLUME:/data $IMAGE sh -c "weasyprint \"/data/$FICH\" \"/data/$NOMFICH.webp\""
 
   # Recuperation des fichiers convertis
@@ -38,4 +38,4 @@ done
 docker rm -f $TRANSFERT >> $LOGS
 
 echo --------------
-echo "Tous les fichiers .png ont été convertis !"
+echo "Tous les fichiers .jpg ont été convertis !"
