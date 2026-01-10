94453d9a42d41d0d819ab61e154d21f49d301845
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-01-10 14:45:03 +0000

    debut de text2html.php

---
+++ text2html.php
@@ -0,0 +1,17 @@
+#!/bin/php
+<?php 
+$fichier = $argv[1];
+
+$lignes = file($fichier);
+
+$tab_html = [];
+foreach ($lignes as $ligne) {
+    $ligne = rtrim($ligne);
+    // On remets les guillemets au debut et a la fin de la premiere case (nom du site)
+    $ligne = 'BAKAA' . $ligne . 'OOOO';
+    // On fusionne les cellules en les séparant par des virgules
+    $tab_html[] = $ligne;
+}
+
+file_put_contents($fichier, implode("\n", $lignes));
+?>
