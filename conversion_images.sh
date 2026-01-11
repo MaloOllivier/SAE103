31b0b5a7b66d29afe58611d79a3964ce72bb17f5
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-01-11 10:23:44 +0000

    conversion_images fonctionne pour agrandir les images

--- conversion_images.sh
+++ conversion_images.sh
@@ -1,4 +1,4 @@
-FICH="test.jpg"
+FICH="Untitled.png"
 CHEMIN=$(pwd)
 LOGS="logs.txt"
 IMAGE="bigpapoo/sae103-imagick"
@@ -10,34 +10,57 @@ TRANSFERT="temporaire_$(date +%s%N)"
 docker run -dit --name "$TRANSFERT" -v "$VOLUME:/data" "$IMAGE" >> "$LOGS"
 docker cp "$FICH" "$TRANSFERT:/data/$FICH" >> "$LOGS"
 
-NOMFICH=$(basename "$FICH" .jpg)
+NOMFICH=$(basename "$FICH" .png)
 
 # Dimensions de l'image
 LARGEUR=$(docker run --rm --entrypoint "" -v "$VOLUME:/data" "$IMAGE" identify -format '%w' /data/$FICH)
 HAUTEUR=$(docker run --rm --entrypoint "" -v "$VOLUME:/data" "$IMAGE" identify -format '%h' /data/$FICH)
 echo "Les dimensions sont h: $HAUTEUR et l: $LARGEUR"
-
-# Calcul du ratio pour respecter les dimensions
 H_MAX=620
 L_MAX=900
-RATIO_L=$(( $L_MAX * 1000 / LARGEUR ))
-RATIO_H=$(( $H_MAX * 1000 / HAUTEUR ))
+H_MIN=250
+L_MIN=350
+if [ $LARGEUR -gt $L_MAX ] || [ $HAUTEUR -gt $H_MAX ]; then
+    # Calcul du ratio pour respecter les dimensions
 
-# Comparer pour prendre le plus petit
-if [ $RATIO_L -lt $RATIO_H ]; then
-    RATIO=$RATIO_L
-else
-    RATIO=$RATIO_H
+    RATIO_L=$(( $L_MAX * 1000 / LARGEUR ))
+    RATIO_H=$(( $H_MAX * 1000 / HAUTEUR ))
+
+    # Comparer pour prendre le plus petit
+    if [ $RATIO_L -lt $RATIO_H ]; then
+        RATIO=$RATIO_L
+    else
+        RATIO=$RATIO_H
+    fi
+
+    # Nouvelle largeur et hauteur
+    L_FINAL=$(( $LARGEUR * $RATIO / 1000 ))
+    H_FINAL=$(( $HAUTEUR * $RATIO / 1000 ))
+    echo "Les nouvelles dimensions sont h: $H_FINAL et l: $L_FINAL"
+
+    # Conversion en webp avec la bonne taille et une qualité réduite
+    docker run --rm --entrypoint "" -v "$VOLUME:/data" "$IMAGE" convert /data/$FICH -resize "${L_FINAL}x${H_FINAL}" -quality 90 /data/$NOMFICH.webp
+elif [ $LARGEUR -lt $L_MIN ] || [ $HAUTEUR -lt $H_MIN ]; then
+   # Calcul du ratio pour respecter les dimensions
+
+    RATIO_L=$(( $L_MIN * 1000 / LARGEUR ))
+    RATIO_H=$(( $H_MIN * 1000 / HAUTEUR ))
+
+    # Comparer pour prendre le plus petit
+    if [ $RATIO_L -lt $RATIO_H ]; then
+        RATIO=$RATIO_L
+    else
+        RATIO=$RATIO_H
+    fi
+
+    # Nouvelle largeur et hauteur
+    L_FINAL=$(( $LARGEUR * $RATIO / 1000 ))
+    H_FINAL=$(( $HAUTEUR * $RATIO / 1000 ))
+    echo "Les nouvelles dimensions sont h: $H_FINAL et l: $L_FINAL"
+
+    # Conversion en webp avec la bonne taille et une qualité réduite
+    docker run --rm --entrypoint "" -v "$VOLUME:/data" "$IMAGE" convert /data/$FICH -resize "${L_FINAL}x${H_FINAL}" /data/$NOMFICH.webp
 fi
-
-# Nouvelle largeur et hauteur
-L_FINAL=$(( $LARGEUR * $RATIO / 1000 ))
-H_FINAL=$(( $HAUTEUR * $RATIO / 1000 ))
-echo "Les nouvelles dimensions sont h: $H_FINAL et l: $L_FINAL"
-
-# Conversion en webp avec la bonne taille et une qualité réduite
-docker run --rm --entrypoint "" -v "$VOLUME:/data" "$IMAGE" convert /data/$FICH -resize "${L_FINAL}x${H_FINAL}" -quality 90 /data/$NOMFICH.webp
-
 
 docker cp "$TRANSFERT:/data/$NOMFICH.webp" "$CHEMIN/resultat/" >> "$LOGS"
 
