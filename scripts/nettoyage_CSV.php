8bea9cde74a5c9ad04a2fffeaeef7e49b7acce75
MaloOllivier <malo.ollivier@etudiant.uni-rennes.fr>
2026-01-11 19:41:16 +0000

    ajout des noms de departements  ENFINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN

--- scripts/nettoyage_CSV.php
+++ scripts/nettoyage_CSV.php
@@ -23,9 +23,9 @@ foreach ($lignes as $ligne) {
     $tableau_propre[] = $cellules_nettoyee; // On met toutes no cellules dans un seul et meme tableau
 }
 
-$lignesDEPTS = file("/data/DEPTS");
-$nbDepts = count($lignesDEPTS);
 
+$lignesDEPTS = file("/data/DEPTS"); // ajout des departements manquants
+$nbDepts = count($lignesDEPTS);
 for ($i = 1; $i < $nbDepts; $i++) {
     $trouve = false;
 
@@ -37,7 +37,7 @@ for ($i = 1; $i < $nbDepts; $i++) {
     }
 
     if ($trouve == false) {
-        $tableau_propre[] = ["", $i, ""]; 
+        $tableau_propre[] = ["", $i, 0]; 
     }
 }
 
@@ -49,6 +49,7 @@ foreach ($tableau_propre as $indice => $ligne) {
     }
 }
 
+
 $lignes_finales = [];
 foreach ($tableau_propre as $ligne_tab) {
     // On fusionne les cellules en les séparant par des virgules
@@ -57,28 +58,57 @@ foreach ($tableau_propre as $ligne_tab) {
 
 file_put_contents($fichier, implode("\n", $lignes_finales) . "\n");
 
-shell_exec("sort -t',' -k 2,2 -n -r \"$fichier\" -o \"$fichier\"");
 
-// On mets le fichier trié dans un tableau propre
-$lignes_triees = file($fichier);
+$tableau_depts = [];
+
+foreach ($tableau_propre as $index => $ligne) { // ajout des noms de departements
+    $nouvelle_ligne = $ligne;
+    // On récupère le code du departement, et on enleve les potentiels espaces
+    $code_dep = trim($ligne[1]); 
+    // Recherche du nom du departement
+    if ($code_dep == "20.1") {
+        $nom_dept = $lignesDEPTS[19]; // Corse-du-Sud
+    } elseif ($code_dep == "20.2") {
+        $nom_dept = $lignesDEPTS[20]; // Haute-Corse
+    } else {
+        $num = $code_dep;
+        if ($num <= 19) {
+            // De 1 à 19 on mets -1 car on commence a 0
+            $nom_dept = $lignesDEPTS[$num - 1];
+        } else {
+            // Apres la corse qui est en double le dept = num
+            $nom_dept = isset($lignesDEPTS[$num]) ? $lignesDEPTS[$num] : "Inconnu";
+        }
+    }
+    // On ajoute le nom du département dans la deuxième colonne
+    array_splice($nouvelle_ligne, 1, 0, trim($nom_dept));
+    
+    // On transforme le tableau pour qu'il soit enregistrable
+    $tableau_depts[] = implode(",", $nouvelle_ligne);
+}
+// Enregistrement + trie
+file_put_contents($fichier, implode("\n", $tableau_depts));
+shell_exec("sort -t',' -k 3,3 -n -r \"$fichier\" -o \"$fichier\"");
+
+
 $tableau_final = [];
-
-foreach ($lignes_triees as $ligne) {
+$tableau_trie= file($fichier); // on le rouvre pour mettre les 2A et 2B de la corse
+foreach ($tableau_trie as $ligne) {
     $ligne = rtrim($ligne);
     $cellules = explode(",", $ligne);
     
     // On remets les bons departements
-    if ($cellules[1] == "20.1") {
-        $cellules[1] = "2A";
+    if ($cellules[2] == "20.1") {
+        $cellules[2] = "2A";
     }
-    if ($cellules[1] == "20.2") {
-        $cellules[1] = "2B";
+    if ($cellules[2] == "20.2") {
+        $cellules[2] = "2B";
     }
     
-    $tableau_final[] = implode(",", $cellules);
+    $tableau_final[] = implode(",",$cellules);
 }
 
-// On enregistre
+// Enregistrement final
 file_put_contents($fichier, implode("\n", $tableau_final));
 echo "✓ $fichier a été nettoyé.\n";
 ?>
